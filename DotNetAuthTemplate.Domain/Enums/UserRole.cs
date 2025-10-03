af7fe61c17618ed35c70dca4500bd2ec010f6fb4
Emirhan Efe Gözpınar <emirhanefegozpinar@gmail.com>
2025-10-03 13:06:08 +0300

    Initialize project structure with Application and Domain layers

---
+++ DotNetAuthTemplate.Domain/Enums/UserRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetAuthTemplate.Domain.Enums
+{
+    public enum UserRole
+    {
+        Admim = 1,
+        User = 0
+    }
+}
